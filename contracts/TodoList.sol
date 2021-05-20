27f4373fb9f9234044ea8d0db29c553ab64ba0e4
vaibhav singh <61928952+Titansingh@users.noreply.github.com>
2021-05-21 03:36:46 +0530

    todo basic ready
    create task funtion and  mapping var to store  data in bc

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList{
     uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task) public task;
+    constructor() public {
+        createTask("Check Out WickedGamer");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        task[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
 
